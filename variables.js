b7379d4cabeb90da9279b616aba0ae8359180ef7
ARUNKARTHIKEYAN M <keyanarunkarthi@gmail.com>
2026-08-17 13:08:49 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,12 @@
+console.log("welcome to jurassic park");
+console.log("welcome to jurassic park");
+console.log("123456");
+console.log("welcome to " + "MCA");
+let name = "sharan";
+console.log("My name is " + name);
+let currentvalue = 10;
+console.log("current value is " + currentvalue);
+currentvalue = 20;
+console.log(" value " + currentvalue);
+currentvalue = 150;//
+console.log(" value " + currentvalue);
