0028a91981a59c097ce98e3537b77a02ca592037
ARUNKARTHIKEYAN M <keyanarunkarthi@gmail.com>
2026-08-17 13:28:42 +0530

    Refactor variables and console logs in variables.js
    Updated name variable and added console logs for current value.

--- variables.js
+++ variables.js
@@ -2,7 +2,7 @@ console.log("welcome to jurassic park");
 console.log("welcome to jurassic park");
 console.log("123456");
 console.log("welcome to " + "MCA");
-let name = "sharan";
+let name = "Arun";
 console.log("My name is " + name);
 let currentvalue = 10;
 console.log("current value is " + currentvalue);
